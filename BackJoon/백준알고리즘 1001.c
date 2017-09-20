cdbef793d862055afce05ff8639431e33980f5e6
tkdals1119 <asb1119@naver.com>
2017-09-20 17:40:47 +0900

    백준1000, 1001 추가

---
+++ BackJoon/백준알고리즘 1001.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int A, B;
+	
+	scanf("%d %d", &A, &B);
+	
+	if(A<=0 || B>=10)
+	{
+		return;
+	}
+	else
+	{
+		printf("%d", A-B);
+	}
+	return 0;
+}
