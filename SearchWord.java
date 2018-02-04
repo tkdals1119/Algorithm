21591b5b4c37169c4ad7bb55bd1632d467b6d90b
tkdals1119 <asb1119@naver.com>
2018-02-04 23:00:30 +0900

    [수정] SearchWord.java(refactoring)

--- SearchWord.java
+++ SearchWord.java
@@ -11,43 +11,30 @@ public class SearchWord {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try{
-			File file = new File("C:\\Users\\SAMSUNG\\Desktop\\sm\\KakaoTalk_20180202_2216_57_878_group.txt");
+			File file = new File("C:\\Users\\SAMSUNG\\Desktop\\sm\\test.txt");
 			FileReader filereader = new FileReader(file);
 			BufferedReader bufReader = new BufferedReader(filereader);
 			String line = "";
 			String res = "";
 			int count = 0;
-			String name[] = {"name1", "name2", "name3", "name4", "name5", "name6", "name7", "name8", "name9", "name10", "name11", "name12", "name13"};
-			int[] count_name = new int[13];
+			int index = 1;
+			String name[] = {"name1", "name2", "name3", "name4", "name5"};
+			int[] count_name = new int[5];
 			Arrays.fill(count_name, 0);
 			
 			while((line=bufReader.readLine())!=null)
 			{
 				if(line.matches(".*word.*"))
 				{
-					for(int i=1; i<4; i++)
+					count++;
+					while(line.charAt(index)!=']')
 					{					
-						if(line.charAt(i)==']') 
-						{
-							for(int j=0; j<count_name.length; j++)
-							{
-								if(res.equals(name[j])) count_name[j]++; 
-							}
-							count++;
-							res="";						
-						   	break;
-						}
-						else res += line.charAt(i);
-						
-						if(i==3)
-						{
-							for(int j=0; j<count_name.length; j++)
-							{
-								if(res.equals(name[j])) count_name[j]++; 
-							}
-							count++;
-							res="";
-						}
+						res += line.charAt(index);
+						index++;
+					}					
+					for(int i=0; i<name.length; i++)
+					{
+						if(name[i].equals(res)) count_name[i]++;
 					}
 				}
 			}
