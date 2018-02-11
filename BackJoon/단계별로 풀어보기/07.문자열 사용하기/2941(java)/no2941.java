74abfe3454c5f142e544ccc765f42a07f6f23a07
tkdals1119 <asb1119@naver.com>
2018-02-11 20:44:23 +0900

    [추가] no2941.java

--- BackJoon/단계별로 풀어보기/07.문자열 사용하기/2941(java)/no2941.java
+++ BackJoon/단계별로 풀어보기/07.문자열 사용하기/2941(java)/no2941.java
@@ -1,24 +1,31 @@
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class no2941 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String alpha[] = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 		String str;
+		String chk="";
 		int count = 0;
 		int sum = 0;
 		Scanner sc = new Scanner(System.in);
 		str = sc.nextLine();
-		
+			
 		for(int i=0; i<alpha.length; i++)
 		{
-			if(str.contains(alpha[i])) 
+			Pattern pattern = Pattern.compile(alpha[i]);
+			Matcher matcher = pattern.matcher(str);
+			while(matcher.find()) 
 			{
 				count++;
-				sum+=alpha[i].length();
+				str = str.replaceFirst(alpha[i], "1");
 			}
 		}
 		
-		System.out.println(count+(str.length()-sum));
+		str = str.replaceAll("1", "");
+		System.out.println(count+str.length());
+
 	}
 }
